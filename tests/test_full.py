6d4feefd85860625ae777ad99ed66d309dd66e03
Björn Tropf <bjoern.tropf@lebihan.de>
2024-05-12 10:07:44 +0200

    Split the tests

--- tests/test_full.py
+++ tests/test_full.py
@@ -2,20 +2,29 @@
 from pdf_microarray.pdf_microarray import PDFMicroarray
 
 
-def full_test():
+def test_process():
     PDFMicroarray.process(
-        ".debug/documents/sample",
-        ".debug/processed/sample",
+        ".debug/documents",
+        ".debug/processed",
     )
 
+    assert True
+
+
+def test_analyze():
     PDFMicroarray.analyze(
-        ".debug/processed/sample",
+        ".debug/processed",
         ".debug/documents/words.txt",
         ".debug/processed/data.csv",
     )
 
+    assert True
+
+
+def test_plot():
     PDFMicroarray.plot(
         ".debug/processed/data.csv",
+        ".debug/processed/plot.png",
     )
 
     assert True
